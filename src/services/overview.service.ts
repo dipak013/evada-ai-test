c642ff242c60201e5be99553085b630822016b51
dipak013 <dk358693@gmail.com>
2026-06-08 19:07:51 +0530

    Delete src directory

--- src/services/overview.service.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "@/lib/axios";
-
-export const OverviewService = {
-  getOverview: (params: Record<string, any> = {}) => api.get("/overview/", { params }),
-};
-
-export default OverviewService;
